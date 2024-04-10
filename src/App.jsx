9c7dcbc16fa694e52d7aea411bd23bb257600dd5
awhr95 <awhr95@gmail.com>
2024-04-10 10:16:11 +0100

    Edit path

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="" element={<Homepage />} />
+        <Route path="/homepage" element={<Homepage />} />
       </Routes>
     </BrowserRouter>
   );
