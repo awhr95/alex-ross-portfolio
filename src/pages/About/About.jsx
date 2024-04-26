074ac63630bb33c5b61e552138750e822f645159
awhr95 <awhr95@gmail.com>
2024-04-26 12:00:21 +0100

    Add cv download

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,13 +1,16 @@
+import React from "react";
 import "./About.scss";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
+// import cv from "../../assets/documents/alex-ross-cv.pdf";
 
 function About() {
   return (
     <>
       <Header />
-      <div>hello world</div>
-      {/* <GithubRepoViewer username="awhr95" repository="alex-ross-portfolio" /> */}
+      <a href="/alex-ross-cv.pdf" download="alex-ross-cv.pdf">
+        Download
+      </a>
       <Footer />
     </>
   );
