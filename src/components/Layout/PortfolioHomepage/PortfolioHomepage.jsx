86f1ad9b5af88aedd2ee85bbaaa905fa5a97535a
awhr95 <awhr95@gmail.com>
2025-07-12 18:56:22 +0100

    Refactor of Portfolio and Tech Stack plus adding Swiper and Framer Motion

--- src/components/Layout/PortfolioHomepage/PortfolioHomepage.jsx
+++ src/components/Layout/PortfolioHomepage/PortfolioHomepage.jsx
@@ -1,77 +1,21 @@
 import "./PortfolioHomepage.scss";
-import Homepage from "../../../assets/imgs/Homepage.png";
-import Map from "../../../assets/imgs/Map.png";
-import Login from "../../../assets/imgs/Login.png";
-import Backofthenet from "../../../assets/imgs/backofthenet/Backofthenethomepage.png";
-import Gameplay from "../../../assets/imgs/backofthenet/Gameplay.png";
-import SelectLeague from "../../../assets/imgs/backofthenet/SelectLeague.png";
-import Winner from "../../../assets/imgs/backofthenet/Winnercrop.png";
-import Leaderboard from "../../../assets/imgs/backofthenet/Leaderboard.png";
 import Project from "../Project/Project";
+import projects from "../../../data/projects";
 
 const PortfolioHomepage = () => {
-  const NListImages = [
-    {
-      src: Homepage,
-    },
-    {
-      src: Map,
-    },
-    { src: Login },
-  ];
-  const BackOfTheNet = [
-    {
-      src: Backofthenet,
-    },
-    {
-      src: Gameplay,
-    },
-    {
-      src: SelectLeague,
-    },
-    {
-      src: Winner,
-    },
-    {
-      src: Leaderboard,
-    },
-  ];
-
   return (
-    <section className="portfolio">
-      <Project
-        title="NList"
-        description={[
-          "NList is a place to discover and promote volunteering opportunities. The 'air-bnb' of volunteering. I originally built this over a two weeks for my 'Capstone Project' at the end of my coding bootcamp. It is a full stack application with a react front end, a node & express backend with a fully implemented database.",
-          "The pain point NList is built to solve is double sided - to connect volunteers with opportunities, and to connect people or organisations with volunteers. It is a place to discover and promote volunteering opportunities.",
-          "I loved building this app, speaking with potential users to get annecdoctal evidence of the needs for this app, and then building it to solve that problem. I also loved the challenge of building a full stack application in a short time frame, and the satisfaction of seeing it come together.",
-          "The main challenges I found in building it was the building of a data base to suit the needs of the app, something I had never done before.",
-        ]}
-        details={[
-          "Front end: React, Sass, MapBox",
-          "Back end: Node, Express, Knex, JSON Web Token",
-          "Project Management: Figma, Jira, Git",
-        ]}
-        githublink="https://github.com/awhr95/Capstone-nlist-client"
-        images={NListImages}
-      />
-      <Project
-        title="Back of the Net"
-        description={[
-          "A 24-hour hackathon project – a football quiz where you try to name the top scorers in all of Europe’s top 5 leagues.",
-          "I built this project solo in 24 hours for a hackathon. I wanted to build something that would be fun to play, and that would challenge my knowledge of football. I also wanted to build something that would be visually appealing and easy to use.",
-          "I loved the challenge of building this project in such a short time frame, and the satisfaction of seeing it come together. I also loved the challenge of building a project that would be fun to play, and that would challenge my knowledge of football.",
-          "The main challenges I found in building it were the time constraints, and the need to build something that would be fun to play. I also found it challenging to build a project that would be visually appealing and easy to use.",
-        ]}
-        details={[
-          "Front end: React, Sass",
-          "Data: Local JSON, useEffect",
-          "Hackathon: Solo build in 24 hours",
-        ]}
-        githublink="https://github.com/awhr95/back-of-the-net"
-        images={BackOfTheNet}
-      />
-    </section>
+    <div className="portfolio">
+      <section className="portfolio__container">
+        <h1 className="portfolio__title">My Projects</h1>
+        {projects.map((project, index) => (
+          <Project
+            key={project.id}
+            {...project}
+            isOpen={index === 0} // Open the first project by default
+          />
+        ))}
+      </section>
+    </div>
   );
 };
 
---
+++ src/data/techStackData.js
@@ -0,0 +1,55 @@
+import HTML from "../assets/icons/skill-icons--html.svg";
+import ReactIcon from "../assets/icons/skill-icons--react-dark.svg";
+import Sass from "../assets/icons/skill-icons--sass.svg";
+import Javascript from "../assets/icons/skill-icons--javascript.svg";
+import CSS from "../assets/icons/skill-icons--css.svg";
+import Node from "../assets/icons/skill-icons--nodejs-dark.svg";
+import Git from "../assets/icons/skill-icons--git.svg";
+import Express from "../assets/icons/skill-icons--expressjs-light.svg";
+import Knex from "../assets/icons/devicon--knexjs-wordmark.svg";
+import MySQL from "../assets/icons/logos--mysql.svg";
+import TypeScriptIcon from "../assets/icons/skill-icons--typescript.svg";
+import Tailwind from "../assets/icons/skill-icons--tailwindcss-dark.svg";
+import GoLang from "../assets/icons/skill-icons--golang.svg";
+import Python from "../assets/icons/logos--python.svg";
+
+export const techStack = [
+  {
+    title: "HTML",
+    link: "https://developer.mozilla.org/en-US/docs/Web/HTML",
+    icon: HTML,
+  },
+  {
+    title: "CSS",
+    link: "https://www.w3schools.com/css/css_intro.asp",
+    icon: CSS,
+  },
+  { title: "React", link: "https://react.dev/", icon: ReactIcon },
+  { title: "Sass", link: "https://sass-lang.com/", icon: Sass },
+
+  {
+    title: "JavaScript",
+    link: "https://www.javascript.com",
+    icon: Javascript,
+  },
+  { title: "Node.js", link: "https://nodejs.org", icon: Node },
+  { title: "Git", link: "https://git-scm.com", icon: Git },
+  { title: "Express.js", link: "https://expressjs.com", icon: Express },
+  { title: "Knex.js", link: "http://knexjs.org", icon: Knex },
+  { title: "MySQL", link: "https://www.mysql.com", icon: MySQL },
+];
+
+export const learning = [
+  {
+    title: "TypeScript",
+    link: "https://www.typescriptlang.org/",
+    icon: TypeScriptIcon,
+  },
+  { title: "Tailwind", link: "https://tailwindcss.com/", icon: Tailwind },
+  { title: "Go", link: "https://go.dev/", icon: GoLang },
+  {
+    title: "Python",
+    link: "https://python.org/",
+    icon: Python,
+  },
+];
